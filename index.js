2622ddf01cb161a8c705f736042ab7d432553282
RochaPL <pedrolr.contato@gmail.com>
2024-02-17 16:41:32 -0300

    atualização do desafio

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log ("Olá")
+let nome = "Apolo, The cat";
+let nivel = 10001;
+let contadorLinha = 0;
+let rank = [
+    ["Ferro", 0, 1000],
+    ["Bronze", 1001, 2000],
+    ["Prata", 2001, 5000],
+    ["Ouro", 5001, 7000],
+    ["Platina", 7001, 8000],
+    ["Ascendente", 8001, 9000],
+    ["Imortal", 9001, 10000],
+    ["Radiante", 10001, "máx"]
+]
+
+while (contadorLinha < 8){
+    if (nivel >= rank[contadorLinha][1]){
+        if(contadorLinha === 7){
+
+        console.log ("O Herói de nome " + nome + " está no nível de " + rank[contadorLinha][0]);
+        break;
+
+        }
+
+        if (nivel <= rank[contadorLinha][2]){ 
+
+        console.log ("O Herói de nome " + nome + " está no nível de " + rank[contadorLinha][0]);
+        break;
+
+        }
+    
+    }
+    contadorLinha++;
+}
